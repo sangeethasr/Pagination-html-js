7ca2b18cd3829786e4acb5bb2834dffaefa5d257
Sangeetha SR <sangeetharajan567@gmail.com>
2023-07-29 15:09:18 +0530

    Changed pagination logic

--- pagination.js
+++ pagination.js
@@ -1,6 +1,11 @@
 const paginationContainer = document.getElementById("paginationContainer");
 const paginationLinkNext = document.getElementById("paginationLinkNext");
 const paginationLinkPrev = document.getElementById("paginationLinkPrev");
+const paginationLink = document.querySelectorAll(".paginationLink");
+
+const getInventories = async (page) => {
+  console.log("getInventories : ", page);
+};
 
 let currentPage = 1;
 let currentIndex = 0;
@@ -12,11 +17,14 @@ pagination(totalPages, 1);
 function pagination(totalPages, index) {
   if (totalPages < 5) {
     for (let i = 1; i <= totalPages; i++) {
-      paginationContainer.innerHTML += `<a href="#" class="paginationLink">${i}</a>`;
+      paginationLink[i - 1].innerHTML = i;
+      paginationLink[i - 1].classList.add("displayPaginationLink");
     }
   } else if (totalPages >= 5) {
-    for (let i = index; i <= 4 + index; i++) {
-      paginationContainer.innerHTML += `<a href="#" class="paginationLink">${i}</a>`;
+    for (let i = 0; i <= 4; i++) {
+      paginationLink[i].innerHTML = index;
+      paginationLink[i].classList.add("displayPaginationLink");
+      index++;    
     }
   }
   document
@@ -26,13 +34,14 @@ function pagination(totalPages, index) {
     document.querySelectorAll(".paginationLink")[currentIndex].innerText;
 }
 
-const paginationLink = document.querySelectorAll(".paginationLink");
+// const paginationLink = document.querySelectorAll(".paginationLink");
 
 paginationLink.forEach((link, index) => {
   link.addEventListener("click", (e) => {
     e.preventDefault();
     currentIndex = index;
     currentPage = link.innerText;
+    getInventories(Number(currentPage));
     paginationLink.forEach((link) => link.classList.remove("activePagination"));
     link.classList.add("activePagination");
   });
@@ -48,29 +57,29 @@ paginationLinkPrev.addEventListener("click", (e) => {
     paginationLink.forEach((link) => link.classList.remove("activePagination"));
     paginationLink[currentIndex].classList.add("activePagination");
     currentPage = paginationLink[currentIndex].innerHTML;
+    getInventories(Number(currentPage));
   } else if (currentIndex === 0) {
     if (currentPage > 1) {
-      paginationContainer.innerHTML = "";
       index = currentPage - 1;
       pagination(totalPages, index);
+      getInventories(Number(currentPage));
     }
   }
 });
 
 paginationLinkNext.addEventListener("click", (e) => {
-  const paginationLink = document.querySelectorAll(".paginationLink");
   e.preventDefault();
   if (currentIndex !== paginationLink.length - 1) {
     currentIndex++;
     paginationLink.forEach((link) => link.classList.remove("activePagination"));
     paginationLink[currentIndex].classList.add("activePagination");
     currentPage = paginationLink[currentIndex].innerText;
+    getInventories(Number(currentPage));
   } else if (currentIndex === paginationLink.length - 1) {
     if (currentPage < totalPages) {
-      console.log(currentPage, totalPages);
-      paginationContainer.innerHTML = "";
       index++;
       pagination(totalPages, index);
+      getInventories(Number(currentPage));
     }
   }
 });
@@ -84,13 +93,31 @@ function handleKeyPress(event) {
   if (event.key === "Enter") {
     if (searchPage.value) {
       const searchValue = Number(searchPage.value);
-      if (searchValue <= totalPages && searchValue >= 5) {
-        const inititalValue = searchValue - 4;
-        paginationContainer.innerHTML = "";
-        for (let i = inititalValue; i <= searchValue; i++) {
-          paginationContainer.innerHTML += `<a href="#" class="paginationLink">${i}</a>`;
+      if (searchValue <= totalPages && searchValue >=1) {
+        getInventories(searchValue);
+        paginationLink.forEach((link) =>
+          link.classList.remove("activePagination")
+        );
+        let borderValue = searchValue - 4;
+        if (borderValue > 0) {
+          searchPagination(borderValue, searchValue);
+        }else if(borderValue <= 0){
+          searchPagination(searchValue, searchValue);
         }
       }
     }
   }
+}
+
+function searchPagination(borderValue, searchValue){
+  for (let i = 0; i <= 4; i++) {
+    paginationLink[i].innerHTML = borderValue;
+    paginationLink[i].classList.add("displayPaginationLink");
+    borderValue++;    
+  }
+  paginationLink.forEach((link) => {
+    if (link.innerHTML === searchValue.toString()) {
+      link.classList.add("activePagination");
+    }
+  });
 }
